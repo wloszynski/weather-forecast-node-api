9f9cd501bf6c7edb58a97398e79fa87dfa93bf24
wloszynski <adrian@wloszynski.pl>
2021-03-05 12:18:04 +0100

    weather endpoint

--- src/routers/weather.js
+++ src/routers/weather.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = new express.Router();
 
-router.get("/weather/:city", async (req, res) => {
-  res.status(201).send(req.params.city);
+const getWeatherData = require("../utils/weather/weatherForecast");
+const getAirQualityData = require("../utils/weather/airQuality");
+
+router.get("/weather/", async (req, res) => {
+  const lat = 54;
+  const lng = 18;
+  const [airQuality, weatherData] = await Promise.all([
+    await getAirQualityData(lat, lng).then((quality) => quality),
+    await getWeatherData(lat, lng),
+  ]);
+
+  res.send([airQuality, weatherData]);
 });
 
 module.exports = router;
